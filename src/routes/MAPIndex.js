36da9a7e8e0fa1c5cf4ed191f90979a633a5dc47
Vincent3054 <ok96305@gmail.com>
2020-11-05 21:53:15 +0800

    11/02 0952 第一更

--- src/routes/MAPIndex.js
+++ src/routes/MAPIndex.js
@@ -14,7 +14,7 @@ import "slick-carousel/slick/slick-theme.css";
 import imgS001 from '../Assets/S001.jpg';
 import imgS002 from '../Assets/S002.jpg';
 import imgS003 from '../Assets/S003.jpg';
-import imgS004 from '../Assets/S004.jpg';
+import imgS005 from '../Assets/S005.jpg';
 
 const { Title, Text } = Typography;
 
@@ -85,17 +85,16 @@ export default class MAPIndex extends Component {
                 <Row style={{ marginLeft: "15%",marginRight:'15%',marginBottom:'5%' }}>
                     <Col flex="300px" style={{ marginLeft: "3%" }} >
                         <div style={{ margin: '10px 0px 5px 20px' }}>
-                            <Link to="Exhibition"><img width={200} src={imgS003}></img></Link>
+                            <Link to="#"><img width={200} src={imgS005}></img></Link>
                         </div>
                     </Col>
                     <Col flex="auto">
-                        <Link to="Exhibition"><Title level={4}><u>繽紛生命-生物多樣性特展</u></Title></Link>
-                        <p style={{ marginBottom: '5px' }}><Text type="secondary">臺中市 , 國立自然科學博物館 , 第四特展室 , 展出日期: 2020-05-20 至 2021-03-21</Text></p>
-                        <p><Text code>　生命　</Text><Text code>　生物　</Text><Text code>　植物　</Text><Text code>　動物　</Text><Text code>　自然　</Text></p>
-                        <p style={{ marginBottom: '5px' }}><Text>生物始之於簡，繼之以變，現之於繁，呈之以美。生物卅多億年的演化，將地球打造成亮麗的行星，這裡也是所</Text></p>
-                        <p style={{ marginBottom: '5px' }}><Text>有過去、現在和未來生物的家。我們沒有另一個行星家園，There is no Planet B。科博館在2020年世界地球</Text></p>
-                        <p style={{ marginBottom: '5px' }}><Text>日五十周年推出「繽紛的生命」特展，以豐富的館藏精品和民間藝術創作，搭配臺灣在地或世界性的研究故事</Text></p>
-                        <p style={{ marginBottom: '5px' }}><Text>與公民科學，呈現生物世界的感性與知性之美。...... <Link to="Exhibition">more</Link></Text></p>
+                        <Link to="#"><Title level={4}><u>現代國畫畫派 2020 現代國畫創作展</u></Title></Link>
+                        <p style={{ marginBottom: '5px' }}><Text type="secondary">臺北市 , 國立臺灣藝術教育館 , 第一、二展覽廳 , 展出日期: 2020-11-14 至 2020-11-25</Text></p>
+                        <p><Text code>　國畫　</Text><Text code>　創作　</Text><Text code>　藝術　</Text><Text code>　現代　</Text><Text code>　藝術　</Text></p>
+                        <p style={{ marginBottom: '5px' }}><Text>現代國畫畫派創立畫家陳銘顯，提倡筆墨、意境、與現代精神的現代國畫，認為理論及觀念要跟隨時代，創作</Text></p>
+                        <p style={{ marginBottom: '5px' }}><Text>題材要反應生活體驗，以創作者的自我感性為主導，將所見、所聞、所感融入畫中。聚焦賞析：以國畫水墨媒</Text></p>
+                        <p style={{ marginBottom: '5px' }}><Text>材，保留傳統國畫筆墨、意境......<Link to="#">more</Link></Text></p>
                     </Col>
                 </Row>
             </Layout>
