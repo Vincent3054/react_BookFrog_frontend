7b46ceb72588dc9fe580c3bbe89619be402148b2
Vincent3054 <ok96305@gmail.com>
2020-11-05 21:26:45 +0800

    鄰近展覽 完成 1105

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -4,50 +4,114 @@ import { List, Avatar, Rate, Row, Col, Divider, Typography, Modal, Button, Resul
 import GoogleMapReact from 'google-map-react';
 import "../mixin/main.css";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-const findCafeLocation = () => {
-    if(mapApiLoaded) {
-      const service = new mapApi.places.PlacesService(mapInstance)
+const MyPositionMarker = ({ text }) => <div>{text}</div>;
 
-      const request = {
-        location: myPosition,
-        radius: 1000,
-        type: ['cafe']
-      };
-
-      service.nearbySearch(request, (results, status) => {
-        if(status === mapApi.places.PlacesServiceStatus.OK) {
-          console.log(results)
-        }
-      })
-    }
-  }
-
-export default class SimpleMap  extends Component {
+export default class SimpleMap extends Component {
     static defaultProps = {
         center: {
-            lat: 25.0216745,
-            lng: 121.5331069
+            lat: 25.0216697,
+            lng: 121.5331016
         },
-        zoom: 18
+        zoom: 15
     };
+    state = {
 
+        mapApiLoaded: false,
+        mapInstance: null,
+        mapApi: null,
+        myPosition: {
+            lat: 25.0216697,
+            lng: 121.5331016,
+        },
+        places: [],
+        name: [],
+
+    }
     render() {
+        const handleCenterChange = () => {
+            // const { mapInstance} = this.state;
+
+            // this.setState({
+            //     lat: mapInstance.center.lat(),
+            //     lng: mapInstance.center.lng()
+            // })
+
+        }
+        const apiHasLoaded = (map, maps) => {
+            this.setState({
+                mapApiLoaded: true,
+                mapInstance: map,
+                mapApi: maps,
+            })
+        };
+
+        // 找咖啡廳
+        const findCafeLocation = () => {
+            const { mapInstance, myPosition, mapApi, name } = this.state;
+
+            const service = new mapApi.places.PlacesService(mapInstance)
+
+            const request = {
+                location: myPosition,
+                radius: 3000,
+                type: ['museum'],
+                name: name,
+            };
+
+            service.nearbySearch(request, (results, status) => {
+
+                if (status === mapApi.places.PlacesServiceStatus.OK) {
+                    this.setState({
+                        places: results,
+                    })
+                    console.log(results)
+
+                }
+
+            })
+
+        }
+        const { places, myPosition } = this.state;
+        const CafeMarker = ({ icon, text }) => (
+            <div>
+                <img style={{ height: '30px', width: '30px' }} src={icon} />
+                <div>{text}</div>
+            </div>
+        )
         return (
             <div style={{ height: 800, width: 1200 }}>
-                <input type="button" value="找咖啡廳" onClick={findCafeLocation} />
                 <GoogleMapReact
-                
-                    bootstrapURLKeys={{ key:'AIzaSyBl97Q2qi59q3XyhAypbV8eD89fxvifLLU' }}
+                    bootstrapURLKeys={{
+                        key: 'AIzaSyBl97Q2qi59q3XyhAypbV8eD89fxvifLLU',
+                        libraries: ['places'] // 要在這邊放入我們要使用的 API
+                    }}
+                    onBoundsChange={handleCenterChange} // 移動地圖邊界時觸發 handleCenterChange
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({ map, maps }) => apiHasLoaded(map, maps)}
+
                 >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Marker"
+                    <MyPositionMarker
+                        lat={myPosition.lat}
+                        lng={myPosition.lng}
+                        text={<img height="30px" src={'https://www.flaticon.com/svg/static/icons/svg/252/252025.svg'} />}
+
                     />
+                    {places.map(item => (
+                        <CafeMarker
+                            icon={item.icon}
+                            key={item.id}
+                            lat={item.geometry.location.lat()}
+                            lng={item.geometry.location.lng()}
+                            text={item.name}
+                            placeId={item.place_id}
+                        />
+                    ))}
                 </GoogleMapReact>
+                <div style={{ textAlign: 'center', marginBottom: '20px' ,marginTop:'20px'}}>
+                    <Button type="primary" size={'large'} onClick={findCafeLocation} >開始搜尋</Button>
+                </div>
             </div>
         )
     }
--- src/routes/MAPIndex.js
+++ src/routes/MAPIndex.js
@@ -5,7 +5,7 @@ import BraftEditor from 'braft-editor';
 import Slider from "react-slick";
 import SimpleMap from '../components/SimpleMap'
 import { List, Avatar, Rate, Row, Col, Divider, Typography, Modal, Button, Result } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
+import { MessageOutlined, LikeOutlined, StarOutlined, LineChartOutlined } from '@ant-design/icons';
 import "../mixin/main.css";
 import "./book.css";
 import 'braft-editor/dist/index.css';
@@ -18,7 +18,12 @@ import imgS004 from '../Assets/S004.jpg';
 
 const { Title, Text } = Typography;
 
-
+const IconText = ({ icon, text }) => (
+    <Space>
+        {React.createElement(icon)}
+        {text}
+    </Space>
+);
 export default class MAPIndex extends Component {
 
     state = {
@@ -66,55 +71,35 @@ export default class MAPIndex extends Component {
 
     render() {
 
+
         return (
             <Layout>
 
                 <Row>
-                    <Title level={3} style={{ marginLeft: '15%', marginRight: "15%" }}>如何去圖書館(公車)</Title>
-                    <Col span={24}  style={{ marginLeft: '15%', marginRight: "15%" }}>
-
+                    <Title level={3} style={{ marginLeft: '15%', marginRight: "15%" }}>鄰近展覽</Title>
+                    <Col span={24} style={{ marginLeft: '15%', marginRight: "15%", marginBottom: "15%" }}>
                         <SimpleMap />
                     </Col>
                 </Row>
+                <Divider />
 
-
-
-
+                <Row style={{ marginLeft: "15%",marginRight:'15%',marginBottom:'5%' }}>
+                    <Col flex="300px" style={{ marginLeft: "3%" }} >
+                        <div style={{ margin: '10px 0px 5px 20px' }}>
+                            <Link to="Exhibition"><img width={200} src={imgS003}></img></Link>
+                        </div>
+                    </Col>
+                    <Col flex="auto">
+                        <Link to="Exhibition"><Title level={4}><u>繽紛生命-生物多樣性特展</u></Title></Link>
+                        <p style={{ marginBottom: '5px' }}><Text type="secondary">臺中市 , 國立自然科學博物館 , 第四特展室 , 展出日期: 2020-05-20 至 2021-03-21</Text></p>
+                        <p><Text code>　生命　</Text><Text code>　生物　</Text><Text code>　植物　</Text><Text code>　動物　</Text><Text code>　自然　</Text></p>
+                        <p style={{ marginBottom: '5px' }}><Text>生物始之於簡，繼之以變，現之於繁，呈之以美。生物卅多億年的演化，將地球打造成亮麗的行星，這裡也是所</Text></p>
+                        <p style={{ marginBottom: '5px' }}><Text>有過去、現在和未來生物的家。我們沒有另一個行星家園，There is no Planet B。科博館在2020年世界地球</Text></p>
+                        <p style={{ marginBottom: '5px' }}><Text>日五十周年推出「繽紛的生命」特展，以豐富的館藏精品和民間藝術創作，搭配臺灣在地或世界性的研究故事</Text></p>
+                        <p style={{ marginBottom: '5px' }}><Text>與公民科學，呈現生物世界的感性與知性之美。...... <Link to="Exhibition">more</Link></Text></p>
+                    </Col>
+                </Row>
             </Layout>
         )
     }
 }
-
-/*
-    蕨類觀察入門
-                        <p style={{marginBottom: '5px'}}><Text >ISBN：9789573245018 </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>作者：<u>郭城孟</u></Text></p>
-                        <p style={{marginBottom: '5px'}}> <Text>繪者：<u>黃崑謀</u> </Text></p>
-                        <p style={{marginBottom: '5px'}}> <Text>出版社：<u>遠流</u> </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>出版日期：2020/01/15 </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>語言：繁體中文 </Text></p>
-*/
-/*
-柴山蕨類植物
-                        <p style={{marginBottom: '5px'}}><Text > ISBN：9789860244762 </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>作者：<u>楊吉壽</u></Text></p>
-                        <p style={{marginBottom: '5px'}}> <Text>出版社：<u>高雄市政府研究發展考核委員會</u> </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>出版日期：2010/08/01 </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>語言：繁體中文 </Text></p>
-*/
-/*
-蕨妙草山
-                        <p style={{marginBottom: '5px'}}><Text > ISBN：9789860304268</Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>作者：<u>陽明山國家公園管理處</u></Text></p>
-                        <p style={{marginBottom: '5px'}}> <Text>出版社：<u>內政部營建署陽明山國家公園管理處  </u> </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>出版日期：2011/12/01 </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>語言：繁體中文 </Text></p>
-*/
-/*
-蕨影：大雪山國家森林遊樂區蕨類植物解說手冊
-                        <p style={{marginBottom: '5px'}}><Text > ISBN：9789860199987 </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>作者：<u>郭城孟、黃俊溢、高美芳</u></Text></p>
-                        <p style={{marginBottom: '5px'}}> <Text>出版社：<u>行政院農業委員會林務局  </u> </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>出版日期：2011/10/01 </Text></p>
-                        <p style={{marginBottom: '5px'}}><Text>語言：繁體中文 </Text></p>
-*/
